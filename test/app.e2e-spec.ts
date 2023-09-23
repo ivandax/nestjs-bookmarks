218759b0525bc554b89edc8dd9c2a805d5425712
ivandax <ivandariogye@gmail.com>
2023-09-23 17:25:48 +0200

    Add basic signin and login e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -19,11 +24,115 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(3334);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3334');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('Should pass');
+  describe('Auth', () => {
+    it('Should fail sign up if email empty', () => {
+      const dto: AuthDto = {
+        email: '',
+        password: 'somePassword',
+      };
+      return pactum
+        .spec()
+        .post('/auth/signup')
+        .withBody(dto)
+        .expectStatus(400)
+        .inspect();
+    });
+
+    it('Should fail sign up if password empty', () => {
+      const dto: AuthDto = {
+        email: 'test@gmail.com',
+        password: '',
+      };
+      return pactum
+        .spec()
+        .post('/auth/signup')
+        .withBody(dto)
+        .expectStatus(400)
+        .inspect();
+    });
+
+    it('Should signup', () => {
+      const dto: AuthDto = {
+        email: 'test@gmail.com',
+        password: 'somePassword',
+      };
+      return pactum
+        .spec()
+        .post('/auth/signup')
+        .withBody(dto)
+        .expectStatus(201)
+        .inspect();
+    });
+
+    it('Should fail login if email empty', () => {
+      const dto: AuthDto = {
+        email: 'test@gmail.com',
+        password: '',
+      };
+      return pactum
+        .spec()
+        .post('/auth/login')
+        .withBody(dto)
+        .expectStatus(400)
+        .inspect();
+    });
+
+    it('Should fail login if password empty', () => {
+      const dto: AuthDto = {
+        email: '',
+        password: 'somePassword',
+      };
+      return pactum
+        .spec()
+        .post('/auth/login')
+        .withBody(dto)
+        .expectStatus(400)
+        .inspect();
+    });
+
+    it('Should fail login if user does not exist', () => {
+      const dto: AuthDto = {
+        email: 'someOtherUser@gmail.com',
+        password: '1234',
+      };
+      return pactum
+        .spec()
+        .post('/auth/login')
+        .withBody(dto)
+        .expectStatus(400)
+        .inspect();
+    });
+
+    it('Should login', () => {
+      const dto: AuthDto = {
+        email: 'test@gmail.com',
+        password: 'somePassword',
+      };
+      return pactum
+        .spec()
+        .post('/auth/login')
+        .withBody(dto)
+        .expectStatus(200)
+        .inspect();
+    });
+  });
+
+  describe('User', () => {
+    it.todo('Should test user');
+  });
+
+  describe('Bookmarks', () => {
+    it.todo('Should test bookmarks');
+  });
 });
